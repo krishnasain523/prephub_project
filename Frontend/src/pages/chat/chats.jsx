23eb50b566461e01b639a5a1fc28322e616f9493
krishna sain <sainkrishna510@gmail.com>
2026-05-10 23:34:38 +0530

    10-09-26

--- Frontend/src/pages/chat/chats.jsx
+++ Frontend/src/pages/chat/chats.jsx
@@ -7,53 +7,60 @@ import rehypeHiglight from "rehype-highlight"
 import remarkGfm from "remark-gfm";
 import "highlight.js/styles/github-dark.css";
 export default function Chats() {
-    const { prevchats,reply } = useContext(mycontext);
-const [ch,setch]=useState({});
- const [latestreply,setlatestreply]=useState(null);
- useEffect(()=>{
-  if(!prevchats?.length) return;
-  if(reply===null)
-  {
-    setlatestreply(null);
-    return
-  }
-  
-  const content=reply.split(" ");
-   let indx=0;
-   const intervel=setInterval(()=>{
-      setlatestreply(content.slice(0,indx+1).join(" "))
-      indx++;
-     if(indx>=content.length)
-     {
-        clearInterval(intervel);
-     }
-   },40)
-   return ()=> clearInterval(intervel);
- },[prevchats])
+    const { prevchats, reply } = useContext(mycontext);
+    const [ch, setch] = useState({});
+    const [latestreply, setlatestreply] = useState(null);
+    useEffect(() => {
+        if (!prevchats?.length) return;
+        if (reply === null) {
+            setlatestreply(null);
+            return
+        }
+
+        const content = reply.split(" ");
+        let indx = 0;
+        const intervel = setInterval(() => {
+            setlatestreply(content.slice(0, indx + 1).join(" "))
+            indx++;
+            if (indx >= content.length) {
+                clearInterval(intervel);
+            }
+        }, 40)
+        return () => clearInterval(intervel);
+    }, [prevchats])
     return (<>
         <div className='chats '>
             {
-                prevchats?.slice(0,-1).map((chat, indx) => (
-                     <div className="userdiv  relative left-20 md:left-20 lg:left-40" key={indx}>
-                        {chat.role === 'user' ? <p className="usermassege">{chat.content}</p> 
-                        :<ReactMarkdown remarkPlugins={remarkGfm} rehypePlugins={rehypeHiglight}>{chat.content}</ReactMarkdown>}
+                prevchats?.slice(0, -1).map((chat, indx) => (
+                    <div className="userdiv  relative left-20 md:left-20 lg:left-40" key={indx}>
+                        {chat.role === 'user' ? <p className="usermassege">{chat.content}</p>
+                            : (
+                                <div className="gptdiv" key={indx}>
+                                    <ReactMarkdown
+                                        remarkPlugins={[remarkGfm]}
+                                        rehypePlugins={[rehypeHiglight]}
+                                    >
+                                        {chat.content}
+                                    </ReactMarkdown>
+                                </div>
+                            )}
                     </div>
 
                 ))
             }
             {
-                prevchats.length>0 &&  (
-               <>
-               { latestreply===null?
-               <div className='gptdiv' key={"non-typing"}>
-                   <ReactMarkdown remarkPlugins={remarkGfm} rehypePlugins={rehypeHiglight}>{prevchats[prevchats.length-1].content}</ReactMarkdown>
-                </div>:
-                <div className='gptdiv' key={"typing"}>
-                   <ReactMarkdown remarkPlugins={remarkGfm} rehypePlugins={rehypeHiglight}>{latestreply}</ReactMarkdown>
-                </div> }
-                </> 
-) }
-        
+                prevchats.length > 0 && (
+                    <>
+                        {latestreply === null ?
+                            <div className='gptdiv' key={"non-typing"}>
+                                <ReactMarkdown remarkPlugins={remarkGfm} rehypePlugins={rehypeHiglight}>{prevchats[prevchats.length - 1].content}</ReactMarkdown>
+                            </div> :
+                            <div className='gptdiv' key={"typing"}>
+                                <ReactMarkdown remarkPlugins={remarkGfm} rehypePlugins={rehypeHiglight}>{latestreply}</ReactMarkdown>
+                            </div>}
+                    </>
+                )}
+
         </div>
     </>)
 }
