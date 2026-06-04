1a1610cc4bbe51b2e7215760c17777a628a4ea07
krishna sain <sainkrishna510@gmail.com>
2026-06-04 20:56:39 +0530

    3-06-26

--- Frontend/src/components/protectedroute.jsx
+++ Frontend/src/components/protectedroute.jsx
@@ -1,12 +1,12 @@
 import React, { useContext } from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 import { AuthContext } from '../context/Authcontext'
-
+import { ScaleLoader } from "react-spinners";
 
 export default function ProtectedRoute() {
 const { user, loading } = useContext(AuthContext)
   if (loading) {
-    return <div>Loading...</div>; 
+    return <ScaleLoader/>; 
   }
 if (!user) return <Navigate to="/login" replace />
 return <Outlet/>;
